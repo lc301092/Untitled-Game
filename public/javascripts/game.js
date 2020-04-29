e06de42080ca3d5bca9176c4a501ce2cdd56b3d7
no lol <jamiemoeller@yahoo.dk>
2020-04-29 12:14:45 +0200

    Store game results in localStorage, helper function to create messages to room/server.

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -11,7 +11,7 @@ let player = {
 	PrintState: PrintState,
 	DrawCard: DrawCard,
 	Stand: Stand,
-	isStanding: false, 
+	isStanding: false,
 	TakeTurn: PlayerTurn
 };
 
@@ -35,7 +35,7 @@ $(document).ready(function () {
 	InitializePlayers(gameManager);
 
 	// player has three actions with corresponding event listeners
-	
+
 	// draw card
 	$('#btn_draw').on('click', function () {
 		activeAgents[0].DrawCard();
@@ -59,37 +59,51 @@ $(document).ready(function () {
 });
 
 function EndTurn(agent, _gameManager = gameManager) {
-	console.log("##################### " + agent.name + " ends their turn #####################");	
+	console.log("##################### " + agent.name + " ends their turn #####################");
 	_gameManager.GiveTurn(_gameManager.inactivePlayer);
 }
 
 
-function FindWinner(agentArray){
-	let winner = agentArray[0].cardSum > agentArray[1].cardSum ? agentArray[0] : 
-		agentArray[0].cardSum == agentArray[1].cardSum ? {name: "Tie."} : agentArray[1];
+function FindWinner(agentArray) {
+	let winner = agentArray[0].cardSum > agentArray[1].cardSum ? agentArray[0] :
+		agentArray[0].cardSum == agentArray[1].cardSum ? { name: "Tie." } : agentArray[1];
 	console.log("WINNER IS: ");
 	console.log(winner.name);
+
+	let resultString = "";
+
+	if (winner.name == "Tie.") {
+		resultString = "Tie.";
+	}
+	if (winner == agentArray[0]) {
+		resultString = "Win.";
+	}
+	if (winner == agentArray[1]) {
+		resultString = "Losses.";
+	}
+
+	storeGameResults(localAgent, resultString);
 }
 
-function ResetStatus(){
-	for (agent in activeAgents){
+function ResetStatus() {
+	for (agent in activeAgents) {
 		agent.cardSum = 0;
 		agent.isStanding = false;
 	}
 }
 
-function InitializePlayers(_gameManager){
-	for (let i = 0; i < activeAgents.length; i++){
+function InitializePlayers(_gameManager) {
+	for (let i = 0; i < activeAgents.length; i++) {
 		let agent = activeAgents[i];
 		agent.gameManager = _gameManager;
-		agent.SetupDeck();	
+		agent.SetupDeck();
 	}
 	gameManager.players = activeAgents;
 }
 
 //GameManaging function - gives turn to a player, that then uses it for something.
-function GiveTurn(agent){
-	if (!agent.isStanding){
+function GiveTurn(agent) {
+	if (!agent.isStanding) {
 		this.inactivePlayer = this.currentPlayersTurn;
 		this.currentPlayersTurn = agent;
 		agent.TakeTurn();
@@ -97,7 +111,7 @@ function GiveTurn(agent){
 	}
 
 	// Both player are standing 
-	if(this.currentPlayersTurn.isStanding){
+	if (this.currentPlayersTurn.isStanding) {
 		FindWinner(activeAgents);
 		ResetStatus();
 		return;
--- public/javascripts/helper functions.js
+++ public/javascripts/helper functions.js
@@ -2,16 +2,61 @@
 function Shuffle(array) {
 	for (let i = array.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+		[array[i], array[j]] = [array[j], array[i]];
 	}
 	return array;
 }
 
 function isBiggerThanOrEqual(number, threshold) {
-    return number >= threshold ? true : false;
+	return number >= threshold ? true : false;
 }
 
 function isBiggerThan(number, threshold) {
-    return number > threshold ? true : false;
+	return number > threshold ? true : false;
 }
 
+//With a given string, makes this string ready for the server to receive it:
+function MakeMessage(messageString, username) {
+	//Maybe access username from localstorage when calling this?
+	let currentRoom = localStorage.getItem(roomName);
+	let messageObject = { message: messageString, identifyingHandle: username, roomName: currentRoom };
+
+	return messageObject;
+}
+
+
+function storeGameResults(localAgent, resultString) {
+	let currentStats = JSON.parse(localStorage.getItem("playerStats"));
+	if (currentStats == null) {
+
+		switch (resultString) {
+			case "Tie.":
+				currentStats = { name: localAgent.name, wins: 0, losses: 0, ties: 1 };
+				break;
+			case "Win.":
+				currentStats = { name: localAgent.name, wins: 1, losses: 0, ties: 0 };
+				break;
+			case "Loss.":
+				currentStats = { name: localAgent.name, wins: 0, losses: 1, ties: 0 };
+				break;
+		}
+
+		localStorage.setItem("playerStats", JSON.stringify(currentStats));
+		return;
+	}
+
+	switch (resultString) {
+		case "Tie.":
+			currentStats.ties += 1;
+			break;
+		case "Win.":
+			currentStats.wins += 1;
+			break;
+		case "Loss.":
+			currentStats.losses += 1;
+			break;
+	}
+	localStorage.setItem("playerStats", JSON.stringify(currentStats));
+
+
+}
