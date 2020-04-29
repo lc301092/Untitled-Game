5f7c4ca2ff3d3ba842e39b66e6f26beec1e4aa33
Luca Casa <lc301092@gmail.com>
2020-04-29 13:06:35 +0200

    views and routes added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('login', {
+		title: 'Express'
+	});
 });
+router.post('/login', function (req, res, next) {
+	//TODO handle login validation here 
+
+
+	let token = "some-kind-of-id-or-token";
+	// if succesfull render the game or lobby page
+	res.render('lobby', {
+		title: 'Express',
+		user: {
+			name: 'MechaHitler',
+			room: 'lobby',
+			token: token
+		}
+	});
+});
+
+router.post('/enter_room', function (req, res, next) {
+	res.render('game', {
+		user: {
+			name: 'MechaHitler',
+			room: 'lobby',
+			token: 'token from earlier'
+		}
+	})
+});
+
 
 module.exports = router;
