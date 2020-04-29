e4f51d5c0ce081c457633c667c2f099d581b2f02
no lol <jamiemoeller@yahoo.dk>
2020-04-29 15:16:38 +0200

    the full circle

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('login', {
-		title: 'Express'
-	});
+  res.render('login', {
+    title: 'Express'
+  });
 });
+
 router.post('/login', function (req, res, next) {
-	//TODO handle login validation here 
+  //TODO handle login validation here 
 
 
-	let token = "some-kind-of-id-or-token";
-	// if succesfull render the game or lobby page
-	res.render('lobby', {
-		title: 'Express',
-		user: {
-			name: 'MechaHitler',
-			room: 'lobby',
-			token: token
-		}
-	});
+  let token = "some-kind-of-id-or-token";
+  // if succesfull render the game or lobby page
+  res.render('lobby', {
+    title: 'Express',
+    user: {
+      name: 'MechaHitler',
+      room: 'lobby',
+      token: token
+    }
+  });
 });
 
-router.post('/enter_room', function (req, res, next) {
-	res.render('game', {
-		user: {
-			name: 'MechaHitler',
-			room: 'lobby',
-			token: 'token from earlier'
-		}
-	})
+router.post('/chat_message', function (req, res, next) {
+  console.log(req.body);
+  let chatMessage = req.body.message;
+  console.log(chatMessage);
+  res.send({ messageBack: chatMessage });
+  res.end();
+  //res.send("The circle of life: " + chatMessage);
 });
 
 
